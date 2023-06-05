84ac0f2a72c24283381a58851566390ca271f665
mohammed abdu rahoof <mohammedabdurahoof527@gmail.com>
2023-06-04 21:51:19 -0700

    layout and header and footer set

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,128 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <!--====== Required meta tags ======-->
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!--====== Title ======-->
+    <title>Edubin - LMS Education HTML Template</title>
+
+    <!--====== Favicon Icon ======-->
+    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}" type="image/png">
+
+    <!--====== Slick css ======-->
+    <link rel="stylesheet" href="{{ asset('assets/css/slick.css') }}">
+
+    <!--====== Animate css ======-->
+    <link rel="stylesheet" href="{{ asset('assets/css/animate.css') }}">
+
+    <!--====== Nice Select css ======-->
+    <link rel="stylesheet" href="{{ asset('assets/css/nice-select.css') }}">
+
+    <!--====== Nice Number css ======-->
+    <link rel="stylesheet" href="{{ asset('assets/css/jquery.nice-number.min.css') }}">
+
+    <!--====== Magnific Popup css ======-->
+    <link rel="stylesheet" href="{{ asset('assets/css/magnific-popup.css') }}">
+
+    <!--====== Bootstrap css ======-->
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+
+    <!--====== Fontawesome css ======-->
+    <link rel="stylesheet" href="{{ asset('assets/css/font-awesome.min.css') }}">
+
+    <!--====== Default css ======-->
+    <link rel="stylesheet" href="{{ asset('assets/css/default.css') }}">
+
+    <!--====== Style css ======-->
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+
+    <!--====== Responsive css ======-->
+    <link rel="stylesheet" href="{{ asset('assets/css/responsive.css') }}">
+
+
+</head>
+
+<body>
+
+    <!--====== PRELOADER PART START ======-->
+
+    <div class="preloader">
+        <div class="loader rubix-cube">
+            <div class="layer layer-1"></div>
+            <div class="layer layer-2"></div>
+            <div class="layer layer-3 color-1"></div>
+            <div class="layer layer-4"></div>
+            <div class="layer layer-5"></div>
+            <div class="layer layer-6"></div>
+            <div class="layer layer-7"></div>
+            <div class="layer layer-8"></div>
+        </div>
+    </div>
+
+    <!--====== PRELOADER PART START ======-->
+
+    @include('partials.header')
+    @yield('contant')
+    @include('partials.footer')
+
+    <!--====== BACK TO TP PART START ======-->
+
+    <a href="#" class="back-to-top"><i class="fa fa-angle-up"></i></a>
+
+    <!--====== BACK TO TP PART ENDS ======-->
+
+
+
+
+
+
+
+
+    <!--====== jquery js ======-->
+    <script src="{{ asset('assets/js/vendor/modernizr-3.6.0.min.js') }}"></script>
+    <script src="{{ asset('assets/js/vendor/jquery-1.12.4.min.js') }}"></script>
+
+    <!--====== Bootstrap js ======-->
+    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+
+    <!--====== Slick js ======-->
+    <script src="{{ asset('assets/js/slick.min.js') }}"></script>
+
+    <!--====== Magnific Popup js ======-->
+    <script src="{{ asset('assets/js/jquery.magnific-popup.min.js') }}"></script>
+
+    <!--====== Counter Up js ======-->
+    <script src="{{ asset('assets/js/waypoints.min.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.counterup.min.js') }}"></script>
+
+    <!--====== Nice Select js ======-->
+    <script src="{{ asset('assets/js/jquery.nice-select.min.js') }}"></script>
+
+    <!--====== Nice Number js ======-->
+    <script src="{{ asset('assets/js/jquery.nice-number.min.js') }}"></script>
+
+    <!--====== Count Down js ======-->
+    <script src="{{ asset('assets/js/jquery.countdown.min.js') }}"></script>
+
+    <!--====== Validator js ======-->
+    <script src="{{ asset('assets/js/validator.min.js') }}"></script>
+
+    <!--====== Ajax Contact js ======-->
+    <script src="{{ asset('assets/js/ajax-contact.js') }}"></script>
+
+    <!--====== Main js ======-->
+    <script src="{{ asset('assets/js/main.js') }}"></script>
+
+    <!--====== Map js ======-->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDC3Ip9iVC0nIxC6V14CKLQ1HZNF_65qEQ"></script>
+    <script src="{{ asset('assets/js/map-script.js') }}"></script>
+
+</body>
+
+</html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,132 @@
+<!--====== HEADER PART START ======-->
+    
+<header id="header-part">
+    <div class="header-top d-none d-lg-block">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="header-contact">
+                        <ul>
+                            <li><i class="fa fa-envelope"></i><a href="mailto:kpmhaleem528@gmail.com">kpmhaleem528@gmail.com</a></li>
+                            <li><i class="fa fa-phone"></i><a href="tel:+918943485194">8943485194</a></li>
+                        </ul>
+                    </div> <!-- header contact -->
+                </div>
+                <div class="col-md-6">
+                    <div class="header-right d-flex justify-content-end">
+                        <div class="social d-flex">
+                            <span class="follow-us">Follow Us :</span>
+                            <ul>
+                                <li><a href="#"><i class="fa fa-facebook-f"></i></a></li>
+                                <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                            </ul>
+                        </div> <!-- social -->
+                        <div class="login-register">
+                            <ul>
+                                <li><a href="register.html">Login</a></li>
+                                <li><a href="register.html">Register</a></li>
+                            </ul>
+                        </div>
+                    </div> <!-- header right -->
+                </div>
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </div> <!-- header top -->
+    
+    <div class="navigation">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <nav class="navbar navbar-expand-lg">
+                        <a class="navbar-brand" href="index-4.html">
+                            <img src="images/logo.png" alt="Logo">
+                        </a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+
+                        <div class="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
+                            <ul class="navbar-nav ml-auto">
+                                <li class="nav-item">
+                                    <a class="active" href="index.html">Home</a>
+                                    <ul class="sub-menu">
+                                        <li><a class="active" href="index.html">Home 01</a></li>
+                                        <li><a href="index-2.html">Home 02</a></li>
+                                        <li><a href="index-3.html">Home 03</a></li>
+                                        <li><a href="index-4.html">Home 04</a></li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#">Pages</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="about.html">About Us</a></li>
+                                        <li><a href="gallery.html">Gallery</a></li>
+                                        <li><a href="policy.html">Privacy Policy</a></li>
+                                        <li><a href="faq.html">FAQ</a></li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="courses.html">Courses</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="courses.html">Courses</a></li>
+                                        <li><a href="courses-single.html">Course Single</a></li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="events.html">Events</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="events.html">Events List 1</a></li>
+                                        <li><a href="events-2.html">Events List 2</a></li>
+                                        <li><a href="events-single.html">Event Single</a></li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="teachers.html">Teachers</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="teachers.html">teachers</a></li>
+                                        <li><a href="teachers-2.html">teachers 2</a></li>
+                                        <li><a href="teachers-single.html">teacher Single</a></li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="blog.html">Blog</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="blog.html">Blog</a></li>
+                                        <li><a href="blog-single.html">Blog Single</a></li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="shop.html">Shop</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="shop.html">Shop</a></li>
+                                        <li><a href="shop-single.html">Shop Single</a></li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="contact.html">Contact</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="contact.html">Contact Us</a></li>
+                                        <li><a href="contact-2.html">Contact Us 2</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="right-icon text-right">
+                            <ul>
+                                <li><a href="javascript:void(0)" id="search"><i class="fa fa-search"></i></a></li>
+                                <li><a href="#"><i class="fa fa-shopping-bag"></i><span>0</span></a></li>
+                            </ul>
+                        </div> <!-- right icon -->
+                    </nav> <!-- nav -->
+                </div>
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </div>
+</header>
+
+<!--====== HEADER PART ENDS ======-->
