0a126f419eee9fa5406e368a69c806360fcbd59c
Mohammed Abdu Rahoof <mohammedabdurahoof527@gmail.com>
2023-06-05 16:20:58 -0700

    auth added

--- routes/web.php
+++ routes/web.php
@@ -14,57 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('windows.home.index');
+    return view('welcome');
 });
 
-Route::get('/contact', function () {
-    return view('windows.contact.index', ['name' => 'Contact']);
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::get('/about', function () {
-    return view('windows.about.index', ['name' => 'About Us']);
-});
-
-Route::get('/teachers', function () {
-    return view('windows.teachers.index', ['name' => 'Teachers']);
-});
-
-Route::get('/teachers/{id}', function () {
-    return view('windows.teachers.single', ['name' => 'Teachers']);
-});
-
-Route::get('/courses', function () {
-    return view('windows.courses.index', ['name' => 'Courses']);
-});
-
-Route::get('/courses/{id}', function () {
-    return view('windows.courses.single', ['name' => 'Courses']);
-});
-
-Route::get('/events', function () {
-    return view('windows.events.index', ['name' => 'Events']);
-});
-
-Route::get('/events/{id}', function () {
-    return view('windows.events.single', ['name' => 'Events']);
-});
-
-Route::get('/gallery', function () {
-    return view('windows.gallery.index', ['name' => 'Gallery']);
-});
-
-Route::get('/policy', function () {
-    return view('windows.policy.index', ['name' => 'Privacy Policy']);
-});
-
-Route::get('/faq', function () {
-    return view('windows.faq.index', ['name' => 'FAQ']);
-});
-
-Route::get('/login', function () {
-    return view('windows.login.index', ['name' => 'Login']);
-});
-
-Route::get('/register', function () {
-    return view('windows.register.index', ['name' => 'Register']);
-});
+require __DIR__.'/auth.php';
